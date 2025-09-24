ee8fcc38d071a698e8f33f00bc1276d4838d8be3
Osaid <osaidzagha7000@gmail.com>
2025-09-24 15:35:16 +0300

    tailwind changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
 import SavedCitiesPage from './pages/SavedCitiesPage';
+import { useSelector } from 'react-redux';
 
 const App = () => {
+  const theme = useSelector((state) => state.user.theme);
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [theme]);
+
   return (
     <Router>
       <div className="min-h-screen bg-gradient-to-b from-blue-600 to-blue-400 p-6">
